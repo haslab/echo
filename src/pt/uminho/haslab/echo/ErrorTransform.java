f706036bd5220596e1989bcb8fee452cab56a597
nmacedo <nfmmacedo@di.uminho.pt>
2013-02-22 13:09:09 +0000

    QVT2Alloy general cleanup

---
+++ src/pt/uminho/haslab/echo/ErrorTransform.java
@@ -0,0 +1,23 @@
+package pt.uminho.haslab.echo;
+
+public final class ErrorTransform extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private Object object;
+	private String trans;
+
+	public ErrorTransform(String msg, String trans) { super(msg); this.trans = trans; }
+
+	public ErrorTransform(String msg, String trans, Object obj) { 
+		super(msg);
+		object = obj;
+		this.trans = trans;
+	}
+
+	@Override public String toString() {
+		if (object == null) return "Error on transformation "+trans+": "+super.getMessage();
+		else return  "Error transforming "+object.getClass()+ " on "+trans+": "+super.getMessage();
+	}
+
+}
---
+++ src/pt/uminho/haslab/echo/ErrorUnsupported.java
@@ -0,0 +1,23 @@
+package pt.uminho.haslab.echo;
+
+public final class ErrorUnsupported extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private Object object;
+	private String trans;
+
+	public ErrorUnsupported(String msg, String trans) { super(msg); this.trans = trans; }
+
+	public ErrorUnsupported(String msg, String trans, Object obj) { 
+		super(msg);
+		object = obj;
+		this.trans = trans;
+	}
+
+	@Override public String toString() {
+		if (object == null) return "Unsupported object on "+trans+": "+super.getMessage();
+		else return  "Unsupported "+object.getClass()+ " on "+trans+": "+super.getMessage();
+	}
+
+}
