146f3ad256b970bd4a7f5dd68b260f8ae56fc60d
tmguimaraes <tiagommguimaraes26@gmail.com>
2013-03-08 10:06:00 -0800

    Alloy2XMI.java created

---
+++ src/pt/uminho/haslab/echo/transform/Alloy2XMI.java
@@ -0,0 +1,13 @@
+package pt.uminho.haslab.echo.transform;
+
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+
+public class Alloy2XMI {
+	
+	
+	public Alloy2XMI(A4Solution sol, XMI2Alloy oldInst)
+	{
+		;
+	}
+
+}
