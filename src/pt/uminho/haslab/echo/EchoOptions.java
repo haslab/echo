35035fda93bdbe9a62c7051f4ea86519d342ebd0
nmacedo <nfmmacedo@di.uminho.pt>
2013-03-20 21:03:36 +0000

    Better CLI

---
+++ src/pt/uminho/haslab/echo/EchoOptions.java
@@ -0,0 +1,131 @@
+package pt.uminho.haslab.echo;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+public class EchoOptions extends Options{
+
+	private static final long serialVersionUID = 1L;
+	
+	CommandLine cmd;
+
+
+	@SuppressWarnings("static-access")
+	public EchoOptions (String[] args) throws ErrorParser {	
+		super();
+
+		
+		this.addOption(OptionBuilder
+				.withDescription("checkonly mode")
+				.withLongOpt("check")
+				.create("c"));
+		
+		this.addOption(OptionBuilder
+				.hasArg()
+				.withArgName("direction")
+				.withDescription("enforce mode")
+				.withLongOpt("enforce")
+				.create("e"));
+
+		this.addOption(OptionBuilder
+				.withDescription("verbose output")
+				.withLongOpt("verbose")
+				.create());
+		
+		this.addOption(OptionBuilder.withArgName("path")
+				.hasArg()
+				.isRequired(true)
+				.withDescription("the QVT-R transformation file")
+				.withLongOpt("qvtr")
+				.create("q"));
+	
+		this.addOption(OptionBuilder.withDescription("prints this message")
+				.withLongOpt("help")
+				.create("h"));
+	
+		this.addOption(OptionBuilder.withArgName("paths")
+				.withValueSeparator(' ')
+				.withLongOpt("instances")
+				.hasArgs()
+				.isRequired(true)
+				.withDescription("instances of the transformation (should be in the same order as the QVT-R tranformation's arguments)")
+				.create("i"));
+		this.addOption(OptionBuilder.withArgName("paths")
+				.withValueSeparator(' ')
+				.withLongOpt("models")
+				.hasArgs()
+				.isRequired(true)
+				.withDescription("the metamodels of the instances being transformed")
+				.create("m"));
+		
+		this.addOption(OptionBuilder
+				.withDescription("checkonly mode")
+				.withLongOpt("check")
+				.create("c"));
+
+		this.addOption(OptionBuilder
+				.hasArg()
+				.withArgName("nat")
+				.withDescription("maximum delta")
+				.withLongOpt("delta")
+				.withType(Integer.class)
+				.create("d"));
+		
+		CommandLineParser parser = new PosixParser();
+		try {
+			cmd = parser.parse( this, args);
+		} catch (ParseException e) {
+			if (this.isHelp()) {}
+			else throw new ErrorParser(e.getMessage(),"CLI Parser");
+		}
+		
+	}
+
+	public boolean isVerbose() {
+		return cmd.hasOption("verbose");
+	}
+	
+	public boolean isCheck() {
+		return cmd.hasOption("c");
+	}
+
+	public boolean isHelp() {
+		return cmd.hasOption("h");
+	}
+		
+	public String getDirection() {
+		return cmd.getOptionValue("e");
+	}
+	
+	public String getQVTPath() {
+		return cmd.getOptionValue("q");
+	}
+	
+	public String[] getModels() {
+		return cmd.getOptionValues("m");
+	}
+
+	public String[] getInstances() {
+		return cmd.getOptionValues("i");
+	}
+
+	public Integer getMaxDelta() throws ErrorParser {
+		Integer delta = Integer.MAX_VALUE;
+		try {
+			if (cmd.hasOption("d"))
+				delta = (Integer) cmd.getParsedOptionValue("d");
+		} catch (ParseException e) { throw new ErrorParser(e.getMessage(), "CLI Parser");}
+		return delta;
+	}
+	
+	public void printHelp(){
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( "echo [options] {-c|-e <direction>} -q <path> -m <paths>... -i <paths>...", this,false );
+	}
+	
+}
