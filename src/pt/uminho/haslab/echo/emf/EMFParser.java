1d76f091c41961c25e95eee15cf3db2f255ee9d5
nmacedo <nfmmacedo@di.uminho.pt>
2013-04-02 20:01:55 +0100

    Parsers modularized

---
+++ src/pt/uminho/haslab/echo/emf/EMFParser.java
@@ -0,0 +1,135 @@
+package pt.uminho.haslab.echo.emf;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Map.Entry;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.ocl.examples.pivot.utilities.PivotUtil;
+import org.eclipse.ocl.examples.xtext.base.utilities.BaseCSResource;
+import org.eclipse.ocl.examples.xtext.base.utilities.CS2PivotResourceAdapter;
+import org.eclipse.qvtd.pivot.qvtrelation.RelationModel;
+import org.eclipse.qvtd.pivot.qvtrelation.RelationalTransformation;
+import org.eclipse.qvtd.xtext.qvtrelation.QVTrelationStandaloneSetup;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
+import pt.uminho.haslab.echo.ErrorParser;
+
+import com.google.inject.Injector;
+
+public class EMFParser {
+
+	
+	private ResourceSet load_resourceSet = new ResourceSetImpl();
+
+	// the transformation metamodels (package name, epackage)
+	private Map<String,EPackage> metamodels = new HashMap<String,EPackage>();
+	// the transformation instances (qvt argument name, eobject)
+	private Map<String,EObject> instances = new HashMap<String,EObject>();
+
+	private Map<String,String> packagepaths = new HashMap<String,String>();
+	
+	private RelationalTransformation rt;
+
+	public EMFParser(){
+		// Register XML Factory implementation to handle files with ecore extension
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(load_resourceSet.getPackageRegistry());
+		load_resourceSet.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+
+		// Register XML Factory implementation to handle files with any extension
+		load_resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*",new XMIResourceFactoryImpl());
+	}
+	
+	public EObject loadObject(String argURI, String var) {
+
+		// Create empty resource with the given URI
+		Resource load_resource = load_resourceSet.getResource(URI.createURI(argURI), true);
+		EObject res = load_resource.getContents().get(0);
+		instances.put(var,res);	
+
+		return res;
+	}
+	
+	public EPackage loadPackage(String uri)
+	{
+		// Create empty resource with the given URI
+		Resource load_resource = load_resourceSet.getResource(URI.createURI(uri), true);
+		EPackage res = (EPackage) load_resource.getContents().get(0);
+		load_resourceSet.getPackageRegistry().put(res.getNsURI(),res);
+
+		metamodels.put(res.getName(),res);
+	    packagepaths.put(res.getName(), uri);
+	    return res;
+
+	}
+	
+	
+	public RelationalTransformation loadQVT(String uri) throws ErrorParser {
+		CS2PivotResourceAdapter adapter = null;
+
+	//	OCLstdlib.install();
+		QVTrelationStandaloneSetup.doSetup();
+		
+		Injector injector = new QVTrelationStandaloneSetup().createInjectorAndDoEMFRegistration();
+		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		
+		try{
+			Scanner scan = new Scanner(new File(uri));
+			String qvtcontent = scan.useDelimiter("\\Z").next();
+			scan.close();
+			File qvtaux = new File("aux.qvtr");
+			qvtaux.createNewFile();
+			FileWriter out = new FileWriter(qvtaux, true);
+		    BufferedWriter fbw = new BufferedWriter(out);
+		    for (Entry<String,String> mdl : packagepaths.entrySet())
+		    	fbw.write("import "+mdl.getKey()+" : \'"+mdl.getValue()+"\'::"+mdl.getKey()+";\n\n");
+	        fbw.write(qvtcontent);
+	        fbw.close();
+
+			BaseCSResource xtextResource = (BaseCSResource) resourceSet.getResource(URI.createFileURI("aux.qvtr"), true);
+	        qvtaux.delete();
+		
+			String message = PivotUtil.formatResourceDiagnostics(xtextResource.getErrors(), "Error parsing QVT.", "\n\t");
+			if (message != null) throw new ErrorParser (message,"QVT Parser");
+			
+			adapter = CS2PivotResourceAdapter.getAdapter(xtextResource, null);
+			Resource pivotResource = adapter.getPivotResource(xtextResource);
+					
+			RelationModel rm = (RelationModel) pivotResource.getContents().get(0);
+			rt = (RelationalTransformation) rm.eContents().get(0);
+			return rt;
+		} catch (Exception e) { throw new ErrorParser (e.getMessage(),"QVT Parser");}
+
+	}
+	
+	public RelationalTransformation getTransformation(){
+		return rt;
+	}
+
+	public Collection<EPackage> getPackages(){
+		return metamodels.values();
+	}
+
+	public EObject getObjectVar(String var){
+		return instances.get(var);
+	}
+
+}
---
+++ src/pt/uminho/haslab/echo/emf/OCLUtil.java
@@ -0,0 +1,65 @@
+package pt.uminho.haslab.echo.emf;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.ocl.examples.pivot.IfExp;
+import org.eclipse.ocl.examples.pivot.IteratorExp;
+import org.eclipse.ocl.examples.pivot.OCLExpression;
+import org.eclipse.ocl.examples.pivot.OperationCallExp;
+import org.eclipse.ocl.examples.pivot.PrimitiveLiteralExp;
+import org.eclipse.ocl.examples.pivot.PropertyCallExp;
+import org.eclipse.ocl.examples.pivot.VariableDeclaration;
+import org.eclipse.ocl.examples.pivot.VariableExp;
+import org.eclipse.qvtd.pivot.qvtrelation.RelationCallExp;
+import org.eclipse.qvtd.pivot.qvttemplate.ObjectTemplateExp;
+import org.eclipse.qvtd.pivot.qvttemplate.PropertyTemplateItem;
+
+import pt.uminho.haslab.echo.ErrorTransform;
+import pt.uminho.haslab.echo.ErrorUnsupported;
+
+
+
+public class OCLUtil {
+
+	
+	// retrieves the list of variable occurrences of an OCL expression (very incomplete)
+	public static Set<VariableDeclaration> variablesOCLExpression (OCLExpression exp) throws ErrorUnsupported, ErrorTransform {
+		Set<VariableDeclaration> vars = new HashSet<VariableDeclaration>();
+		if (exp == null) return vars;
+		if (exp instanceof VariableExp) vars.add(((VariableExp) exp).getReferredVariable()); 
+		else if (exp instanceof ObjectTemplateExp) {
+			vars.add(((ObjectTemplateExp) exp).getBindsTo()); 
+			for (PropertyTemplateItem part : ((ObjectTemplateExp) exp).getPart())
+				vars.addAll(variablesOCLExpression(part.getValue() ) );
+			vars.addAll(variablesOCLExpression(((ObjectTemplateExp) exp).getWhere())); 
+		}
+		else if (exp instanceof RelationCallExp) {
+			for (OCLExpression e : ((RelationCallExp) exp).getArgument())
+				vars.addAll(variablesOCLExpression(e));
+		}
+		else if (exp instanceof OperationCallExp) {
+			for (OCLExpression e : ((OperationCallExp) exp).getArgument())
+				vars.addAll(variablesOCLExpression(e));
+			vars.addAll(variablesOCLExpression(((OperationCallExp) exp).getSource()));
+		}
+		else if (exp instanceof PropertyCallExp) {
+			vars.addAll(variablesOCLExpression(((PropertyCallExp) exp).getSource()));
+		}
+		else if (exp instanceof IteratorExp) {
+			vars.addAll(variablesOCLExpression(((IteratorExp) exp).getSource()));
+			vars.addAll(variablesOCLExpression(((IteratorExp) exp).getBody()));
+			vars.removeAll(((IteratorExp) exp).getIterator());
+		}
+		else if (exp instanceof IfExp) {
+			vars.addAll(variablesOCLExpression(((IfExp) exp).getCondition()));
+			vars.addAll(variablesOCLExpression(((IfExp) exp).getThenExpression()));
+			vars.addAll(variablesOCLExpression(((IfExp) exp).getElseExpression()));
+		}
+		else if (exp instanceof PrimitiveLiteralExp) {}
+		else throw new ErrorUnsupported ("OCL expression not supported.","OCLUtil",exp);
+
+		return vars;
+	}
+	
+}
