f78b7da63bdb31e75fed29a384e3028fc2fe83b6
tmguimaraes <tiagommguimaraes26@gmail.com>
2013-05-15 16:13:48 +0100

    Marker updated

---
+++ src/pt/uminho/haslab/echo/plugin/EchoMarker.java
@@ -0,0 +1,7 @@
+package pt.uminho.haslab.echo.plugin;
+
+public class EchoMarker {
+	
+	public final static String META_ERROR = "pt.uminho.haslab.echo.plugin.metaconformityrpoblem";
+	public final static String QVT_ERROR = "pt.uminho.haslab.echo.plugin.qvtconformityrpoblem";	
+}
