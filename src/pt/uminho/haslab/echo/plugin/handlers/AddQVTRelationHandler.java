2ae7d961608d31014f3a9c61d9bf5626c405ef35
tmguimaraes <tiagommguimaraes26@gmail.com>
2013-05-26 16:25:02 +0100

    Starting to work on enforce and check

---
+++ src/pt/uminho/haslab/echo/plugin/handlers/AddQVTRelationHandler.java
@@ -0,0 +1,15 @@
+package pt.uminho.haslab.echo.plugin.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class AddQVTRelationHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
