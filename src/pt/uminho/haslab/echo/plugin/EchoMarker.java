087454a1eca1e8c89b20579dd3a2a21104333f77
nmacedo <nfmmacedo@di.uminho.pt>
2013-05-27 12:09:38 +0100

    connect markers with quick fixes

--- src/pt/uminho/haslab/echo/plugin/EchoMarker.java
+++ src/pt/uminho/haslab/echo/plugin/EchoMarker.java
@@ -8,7 +8,10 @@ public class EchoMarker {
 	
 	public final static String INTRA_ERROR = "pt.uminho.haslab.echo.plugin.intrainconsistency";
 	public final static String INTER_ERROR = "pt.uminho.haslab.echo.plugin.interinconsistency";	
-	
+
+	public final static String GED = "ged";
+	public final static String OPS = "ops";	
+
 	public static IMarker createIntraMarker(IResource res) throws CoreException {
 		IMarker mark = res.createMarker(EchoMarker.INTRA_ERROR);
 		mark.setAttribute(IMarker.MESSAGE, "Model instance does not conform to the meta-model.");
