d54022e40ee96ca169394651a64ef0ea39516f8e
Tiago Guimarães <tiagommguimaraes26@gmail.com>
2013-10-29 11:16:11 +0000

    smal change

---
+++ plugins/pt.uminho.haslab.echo/src/pt/uminho/haslab/echo/transform/kodkod/Ecore2Kodkod.java
@@ -0,0 +1,11 @@
+package pt.uminho.haslab.echo.transform.kodkod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tmg
+ * Date: 10/25/13
+ * Time: 4:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ecore2Kodkod {
+}
